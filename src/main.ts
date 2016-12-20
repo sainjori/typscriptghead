18a6e543182bd2a0fd3baa71f2869f6f709b568e
Bertan Yasar <bertan.yasar@dscsag.com>
2016-12-20 12:31:28 +0100

    reflextion and decorator metadata

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-
-function example(){
+import 'reflect-metadata';
+function example() {
     return function (targetClass) {
-/*            Reflect.getMetadata('design:paramtypes', targetClass)*/
-            return targetClass;
+        const types = Reflect.getMetadata('design:paramtypes', targetClass)
+        console.log(types);
+        return targetClass;
+    }
+}
+
+
+@example()
+class Person {
+    constructor(name: string, age:number) {
+
     }
 }
 @example()
-class Person{
+class Cat{
+    constructor(lazy:boolean, hair:any){
+
+    }
 }
-console.log(
-    new Person()
-);
+new Person("john", 10);
+
+
