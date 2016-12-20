d4e701623f181acf3b8eff2d58036ab2192d5af9
James Washington <Jackson@wassub.com>
2016-12-20 01:15:19 +0100

    using typings and loading from node_modules

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import {SocialNetwork} from './social.networks';
 import * as _ from 'lodash';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/interval';
+
+Observable.interval(1000)
+    .subscribe(x => console.log(x));
 class App implements SocialNetwork{
     title = "Eggheads";
 
