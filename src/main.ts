558ab722bde42b9db1478c1254f441b521a9e8cf
Bertan Yasar <bertan.yasar@dscsag.com>
2016-12-20 13:30:29 +0100

    ts definiton files

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,2 @@
-import 'reflect-metadata';
-function example() {
-    return function (targetClass) {
-        const types = Reflect.getMetadata('design:paramtypes', targetClass)
-        console.log(types);
-        return targetClass;
-    }
-}
 
-
-@example()
-class Person {
-    constructor(name: string, age:number) {
-
-    }
-}
-@example()
-class Cat{
-    constructor(lazy:boolean, hair:any){
-
-    }
-}
-new Person("john", 10);
-
-
+import {} from 'lodash';
